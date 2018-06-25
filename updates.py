974ba15920378418d028bd99bd6e84fc912d7d98
Martin Disch <martindisch@gmail.com>
2018-06-25 18:25:33 +0200

    Implement loading of trips, routes and agencies

--- updates.py
+++ updates.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 
 def get_key():
     with open("apikey", "r") as f:
@@ -21,7 +22,43 @@ def get_trip_updates(gtfs_updates):
     return trip_updates
 
 
+def get_trips():
+    with open("gtfs/trips.txt", "r") as f:
+        csv_reader = csv.reader(f)
+        trips = {
+            row[2]: {
+                'route_id': row[0],
+                'trip_headsign': row[3],
+                'trip_short_name': row[4]
+            } for row in csv_reader
+        }
+    return trips
+
+
+def get_routes():
+    with open("gtfs/routes.txt", "r") as f:
+        csv_reader = csv.reader(f)
+        routes = {
+            row[0]: {
+                'agency_id': row[1],
+                'route_short_name': row[2],
+                'route_desc': row[4]
+            } for row in csv_reader
+        }
+    return routes
+
+
+def get_agencies():
+    with open("gtfs/agency.txt", "r") as f:
+        csv_reader = csv.reader(f)
+        agencies = {row[0]: row[1] for row in csv_reader}
+    return agencies
+
+
 if __name__ == "__main__":
     api_key = get_key()
     gtfs_rt_updates = request_gtfs_rt_updates(api_key)
     trip_updates = get_trip_updates(gtfs_rt_updates)
+    trips = get_trips()
+    routes = get_routes()
+    agencies = get_agencies()
