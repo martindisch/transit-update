0fbb54ffe8121e988192d87439332ddb3996765e
Martin Disch <martindisch@gmail.com>
2018-06-25 17:50:27 +0200

    Implement API request

--- updates.py
+++ updates.py
@@ -1,7 +1,18 @@
+import requests
+import json
+
 def get_key():
     with open("apikey", "r") as f:
         key = f.read().strip()
     return key
 
+def request_updates(api_key):
+    r = requests.get(
+        "https://api.opentransportdata.swiss/gtfs-rt?format=JSON",
+        headers={'Authorization': api_key})
+    return json.loads(r.text)
+
 if __name__ == "__main__":
-    print(get_key())
+    api_key = get_key()
+    updates = request_updates(api_key)
+    print(updates['header'])
